1d4cce044d85b0fe3fc7393454b7ab2f16666bad
carlospereira6388 <75950141+carlospereira6388@users.noreply.github.com>
2023-12-29 18:17:18 -0300

    Update DesafioApplication.java

--- src/main/java/br/com/brainit/desafio/DesafioApplication.java
+++ src/main/java/br/com/brainit/desafio/DesafioApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DesafioApplication {
-	
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
 	}
